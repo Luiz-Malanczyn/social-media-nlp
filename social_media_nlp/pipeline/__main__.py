9af86b5d0e171f24d47242a1871bf543f6554dc0
Luiz Eduardo Malanczyn de Oliveira <oliveiraluiz.com@hotmail.com>
2025-04-03 16:38:42 -0300

    feat: implement Twitter data extraction and JSON saving functionality

--- social_media_nlp/pipeline/__main__.py
+++ social_media_nlp/pipeline/__main__.py
@@ -1,4 +1,7 @@
+from social_media_nlp.pipeline.extract.twitter_extractor import TwitterExtractor
+
+
 if __name__ == "__main__":
-    config_loader = ConfigLoaderFactory.create_loader()
-    print("🔹 Configuração do Twitter:", config_loader.get_config("twitter"))
-    print("🔹 Secret do Twitter (bearer_token):", config_loader.get_secret("twitter", "bearer_token"))
+    extractor = TwitterExtractor()
+    tweets = extractor.extract(query="OpenAI", max_tweets=1)
+    extractor.save_to_json(tweets)
--- social_media_nlp/pipeline/extract/twitter_extractor.py
+++ social_media_nlp/pipeline/extract/twitter_extractor.py
@@ -1,3 +1,5 @@
+import json
+import os
 import tweepy
 from script.load_config import load_config
 from extract.base_extractor import BaseExtractor
@@ -18,6 +20,13 @@ class TwitterExtractor(BaseExtractor):
         )
 
     def extract(self, query, max_tweets=100):
-        tweets = self.client.search_recent_tweets(query=query, max_results=max_tweets)
-        return tweets.data if tweets.data else []
+        tweets = self.client.search_recent_tweets(query=query, max_results=max_tweets, tweet_fields=["created_at", "author_id", "text"])
+        tweets_data = tweets.data if tweets.data else []
+        return [{"id": t.id, "author_id": t.author_id, "created_at": t.created_at, "text": t.text} for t in tweets_data]
 
+    def save_to_json(self, data, filename="tweets_bronze.json"):
+        os.makedirs("data/bronze", exist_ok=True)
+        filepath = os.path.join("data/bronze", filename)
+        with open(filepath, "w", encoding="utf-8") as f:
+            json.dump(data, f, ensure_ascii=False, indent=4)
+        print(f"Dados salvos em {filepath}")
